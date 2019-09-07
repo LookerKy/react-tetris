1f28890b11edeb38452a50c5a6235d40a13fc1c9
LookerKy <nagaraz07@gmail.com>
2019-09-07 21:21:17 +0900

    make component

---
+++ src/components/StartButton.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const StartButton = ({ callback }) => <div>GameStart</div>;
+
+export default StartButton;
---
+++ src/gameHelper.js
@@ -0,0 +1,2 @@
+export const MAX_WIDTH = 12;
+export const MAX_HEIGHT = 20;
